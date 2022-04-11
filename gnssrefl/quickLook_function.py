e3236093e807e2092518bdf0d6b2a80aeb1a2881
Tim Dittmann <dittmann@unavco.org>
2022-04-11 14:02:16 -0600

    modify snr parsing logic

--- gnssrefl/quickLook_function.py
+++ gnssrefl/quickLook_function.py
@@ -64,7 +64,7 @@ def read_snr_simple(obsfile, station, year, doy):
             t=(t-start)/(1e6)
             print(t)
             c=10
-            # put in elev mask?
+            # put in positive elev mask? TO DO
         else:
             f = np.genfromtxt(obsfile,comments='%')
             r,c = f.shape
@@ -75,29 +75,26 @@ def read_snr_simple(obsfile, station, year, doy):
             #print('read_snr_simple, Number of rows:', r, ' Number of columns:',c)
             sat = f[:,0]; ele = f[:,1]; azi = f[:,2]; t =  f[:,3]
             edot =  f[:,4]; s1 = f[:,6]; s2 = f[:,7]; s6 = f[:,5]
+            #   make sure s5 has default value?
+            s5=s7=s8 =[]
+            if c > 8:
+                s5 = f[:, 8]
+            if c > 9:
+                s7 = f[:, 9]
+            if c > 10:
+                s8 = f[:, 10]
             #
         s1 = np.power(10,(s1/20))  
         s2 = np.power(10,(s2/20))  
-        s6 = s6/20; s6 = np.power(10,s6)  
-#   make sure s5 has default value?
-        s5 = []
-        if c > 8:
-            s5 = f[:,8]
-            if (sum(s5) > 0):
-                s5 = s5/20; s5 = np.power(10,s5)  
-            #print(len(s5))
-        if c > 9:
-            s7 = f[:,9]
-            if (sum(s7) > 0):
-                s7 = np.power(10,(s7/20))  
-            else:
-                s7 = []
-        if c > 10:
-            s8 = f[:,10]
-            if (sum(s8) > 0):
-                s8 = np.power(10,(s8/20))  
-            else:
-                s8 = []
+        s6 = s6/20; s6 = np.power(10,s6)
+        if (sum(s5) > 0):
+            s5 = s5 / 20;
+            s5 = np.power(10, s5)
+        if (sum(s7) > 0):
+            s7 = np.power(10,(s7/20))
+        if (sum(s8) > 0):
+            s8 = np.power(10,(s8/20))
+
         if (np.sum(s5) == 0):
             snrE[5] = False; #print('no s5 data')
         if (np.sum(s6) == 0):
